763b338d5884be085e6a739b6c53a7fd1904ebe6
danilomartinelli <danilomartinelli429@gmail.com>
2022-06-11 17:17:11 -0300

    Add Confirmation and Password Columns to Users Table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,28 @@
 class User < ApplicationRecord
+  CONFIRMATION_TOKEN_EXPIRY = 1.day
+
+  has_secure_password
+
   before_save :downcase_email
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+
+  def confirm!
+    self.confirmed_at = Time.zone.now
+    save!
+  end
+
+  def confirmed?
+    confirmed_at.present?
+  end
+
+  def unconfirmed?
+    !confirmed?
+  end
+
+  def generate_confirmation_token
+    signed_id expires_in: CONFIRMATION_TOKEN_EXPIRY, purpose: :confirm_email
+  end
 
   private
 

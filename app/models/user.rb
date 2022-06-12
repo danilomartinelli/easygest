41545f33662627ce736331df113c668ebac11a36
danilomartinelli <danilomartinelli429@gmail.com>
2022-06-12 11:18:08 -0300

    Create Sign Up Pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
   CONFIRMATION_TOKEN_EXPIRY = 1.day
 
-  has_secure_password
+  has_secure_password validations: false
 
   before_save :downcase_email
 
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Email inválido" },
+                    presence: { message: "Email é obrigatório" },
+                    uniqueness: { message: "Email já cadastrado" }
+
+  validates :password, presence: { message: "Senha é obrigatório" },
+                       length: { minimum: 8,
+                                 message: "Sua senha deve ter pelo menos 8 caracteres" }
+
+  validates :password, confirmation: { allow_blank: true, message: "Deve ser igual a senha" }
 
   def confirm!
     self.confirmed_at = Time.zone.now
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
+
+  post "cadastrar", to: "users#create", as: :new_user
+  get "cadastrar", to: "users#new", as: :new_user_view
 end

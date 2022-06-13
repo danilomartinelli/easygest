6b9ef496efc019c05abe444f1d715e247bcafb15
danilomartinelli <danilomartinelli429@gmail.com>
2022-06-13 10:18:11 -0300

    added sentry

--- Gemfile
+++ Gemfile
@@ -53,6 +53,10 @@ gem "bootsnap", require: false
 
 gem "bcrypt", "~> 3.1.7"
 
+gem "sentry-ruby"
+
+gem "sentry-rails"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,13 @@
+Sentry.init do |config|
+  config.dsn = 'https://c6868950b0e042b5822cf7c1976f97a1@o1287201.ingest.sentry.io/6501890'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
